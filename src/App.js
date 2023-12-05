864352b0307edcfd01b02dadfff03c1bebf5220c
TomRiddelsdell <t.riddelsdell@gmail.com>
2023-12-05 08:57:36 +0000

    update App.js to hit api

--- src/App.js
+++ src/App.js
@@ -1,58 +1,107 @@
-import logo from './logo.svg';
-/*import './App.css';
-import {Amplify} from 'aws-amplify'
-import config from '../src/aws-exports'
-
-import {AmplifyProvider} from '@aws-amplify/ui-react'
-import {studioTheme} from '../src/ui-components'
-
-import '@aws-amplify/ui-react/styles.css'
-
-Amplify.configure(config)
-
-function App() {
-  return (
-    <AmplifyProvider theme={studioTheme}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-    </div>
-    </AmplifyProvider>
-  );
-export default App;
-}*/
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
+import { API } from "aws-amplify";
 import {
-  withAuthenticator,
   Button,
+  Flex,
   Heading,
-  Image,
+  Text,
+  TextField,
   View,
-  Card,
+  withAuthenticator,
 } from "@aws-amplify/ui-react";
+import { listHabits } from "./graphql/queries";
+import {
+  createHabit as createHabitMutation,
+  deleteHabit as deleteHabitMutation,
+} from "./graphql/mutations";
 
-function App({ signOut }) {
+const App = ({ signOut }) => {
+  const [notes, setHabits] = useState([]);
+
+  useEffect(() => {
+    fetchHabits();
+  }, []);
+
+  async function fetchHabits() {
+    const apiData = await API.graphql({ query: listHabits });
+    const notesFromAPI = apiData.data.listHabits.items;
+    setHabits(notesFromAPI);
+  }
+
+  async function createHabit(event) {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    const data = {
+      name: form.get("name"),
+      description: form.get("description"),
+    };
+    await API.graphql({
+      query: createHabitMutation,
+      variables: { input: data },
+    });
+    fetchHabits();
+    event.target.reset();
+  }
+
+  async function deleteHabit({ id }) {
+    const newHabits = notes.filter((note) => note.id !== id);
+    setHabits(newHabits);
+    await API.graphql({
+      query: deleteHabitMutation,
+      variables: { input: { id } },
+    });
+  }
+
   return (
     <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
+      <Heading level={1}>My Habits App</Heading>
+      <View as="form" margin="3rem 0" onSubmit={createHabit}>
+        <Flex direction="row" justifyContent="center">
+          <TextField
+            name="name"
+            placeholder="Habit Name"
+            label="Habit Name"
+            labelHidden
+            variation="quiet"
+            required
+          />
+          <TextField
+            name="description"
+            placeholder="Habit Description"
+            label="Habit Description"
+            labelHidden
+            variation="quiet"
+            required
+          />
+          <Button type="submit" variation="primary">
+            Create Habit
+          </Button>
+        </Flex>
+      </View>
+      <Heading level={2}>Current Habits</Heading>
+      <View margin="3rem 0">
+        {notes.map((note) => (
+          <Flex
+            key={note.id || note.name}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Text as="strong" fontWeight={700}>
+              {note.name}
+            </Text>
+            <Text as="span">{note.description}</Text>
+            <Button variation="link" onClick={() => deleteHabit(note)}>
+              Delete note
+            </Button>
+          </Flex>
+        ))}
+      </View>
       <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
-}
+};
 
 export default withAuthenticator(App);
